68b6cc0ca8a75486f5b614f33d6e2620bfee4864
prgits <pr.thinhnv@gmail.com>
2017-06-17 23:30:07 +0700

    fd

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,75 +19,67 @@ class Home extends Component {
                 {src: 'slide6.jpeg', label: 'label6'},
               ]}/>
             </div>
-            <div>
+            <div className="col-md-8 well">
               <p>
                 I have successfully solved this problem with javascript files only.
-                Try adding the async attribute in script tag or defer attribute.
-
-                For example:
-
-                <script src="filename.js" async></script>
-                <script src="filename.js" async="async"></script> 
-                or
-
-                <script src="filename.js" defer></script>
-                <script src="filename.js" async="async"></script>
+                Try adding the async attribute in script tag or defer attribute
+                For example
                 I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
               </p>
               <p>
                 I have successfully solved this problem with javascript files only.
-                Try adding the async attribute in script tag or defer attribute.
-
-                For example:
-
-                <script src="filename.js" async></script>
-                <script src="filename.js" async="async"></script> 
-                or
-
-                <script src="filename.js" defer></script>
-                <script src="filename.js" async="async"></script>
+                Try adding the async attribute in script tag or defer attribute
+                For example
                 I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
               </p>
               <p>
                 I have successfully solved this problem with javascript files only.
-                Try adding the async attribute in script tag or defer attribute.
-
-                For example:
-
-                <script src="filename.js" async></script>
-                <script src="filename.js" async="async"></script> 
-                or
-
-                <script src="filename.js" defer></script>
-                <script src="filename.js" async="async"></script>
+                Try adding the async attribute in script tag or defer attribute
+                For example
                 I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
               </p>
               <p>
                 I have successfully solved this problem with javascript files only.
-                Try adding the async attribute in script tag or defer attribute.
-
-                For example:
-
-                <script src="filename.js" async></script>
-                <script src="filename.js" async="async"></script> 
-                or
-
-                <script src="filename.js" defer></script>
-                <script src="filename.js" async="async"></script>
+                Try adding the async attribute in script tag or defer attribute
+                For example
                 I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
               </p>
               <p>
                 I have successfully solved this problem with javascript files only.
-                Try adding the async attribute in script tag or defer attribute.
-
-                For example:
-
-                <script src="filename.js" async></script>
-                <script src="filename.js" async="async"></script> 
-                or
-
-                <script src="filename.js" defer></script>
-                <script src="filename.js" async="async"></script>
+                Try adding the async attribute in script tag or defer attribute
+                For example
+                I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
+              </p>
+            </div>
+            <div className="col-md-8 well">
+              <p>
+                I have successfully solved this problem with javascript files only.
+                Try adding the async attribute in script tag or defer attribute
+                For example
+                I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
+              </p>
+              <p>
+                I have successfully solved this problem with javascript files only.
+                Try adding the async attribute in script tag or defer attribute
+                For example
+                I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
+              </p>
+              <p>
+                I have successfully solved this problem with javascript files only.
+                Try adding the async attribute in script tag or defer attribute
+                For example
+                I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
+              </p>
+              <p>
+                I have successfully solved this problem with javascript files only.
+                Try adding the async attribute in script tag or defer attribute
+                For example
+                I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
+              </p>
+              <p>
+                I have successfully solved this problem with javascript files only.
+                Try adding the async attribute in script tag or defer attribute
+                For example
                 I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
               </p>
             </div>
