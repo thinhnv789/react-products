1021854268aca5dc71766ed7b5c070f91d6ba7e3
prgits <pr.thinhnv@gmail.com>
2017-06-17 23:45:34 +0700

    fd

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,16 @@ import ImageSlider from '../ImageSlider/ImageSlider';
 
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDiv: false
+    }
+  }
+  componentDidMount() {
+    setTimeout(
+      function() { this.setState({showDiv: true}); }.bind(this), 5000)
+  }
   render() {
     return (
       <div className="homepage">
@@ -51,38 +61,42 @@ class Home extends Component {
                 I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
               </p>
             </div>
-            <div className="col-md-8 well">
-              <p>
-                I have successfully solved this problem with javascript files only.
-                Try adding the async attribute in script tag or defer attribute
-                For example
-                I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
-              </p>
-              <p>
-                I have successfully solved this problem with javascript files only.
-                Try adding the async attribute in script tag or defer attribute
-                For example
-                I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
-              </p>
-              <p>
-                I have successfully solved this problem with javascript files only.
-                Try adding the async attribute in script tag or defer attribute
-                For example
-                I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
-              </p>
-              <p>
-                I have successfully solved this problem with javascript files only.
-                Try adding the async attribute in script tag or defer attribute
-                For example
-                I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
-              </p>
-              <p>
-                I have successfully solved this problem with javascript files only.
-                Try adding the async attribute in script tag or defer attribute
-                For example
-                I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
-              </p>
-            </div>
+            {
+              (this.state.showDiv) ? (
+                <div className="col-md-8 well">
+                  <p>
+                    I have successfully solved this problem with javascript files only.
+                    Try adding the async attribute in script tag or defer attribute
+                    For example
+                    I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
+                  </p>
+                  <p>
+                    I have successfully solved this problem with javascript files only.
+                    Try adding the async attribute in script tag or defer attribute
+                    For example
+                    I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
+                  </p>
+                  <p>
+                    I have successfully solved this problem with javascript files only.
+                    Try adding the async attribute in script tag or defer attribute
+                    For example
+                    I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
+                  </p>
+                  <p>
+                    I have successfully solved this problem with javascript files only.
+                    Try adding the async attribute in script tag or defer attribute
+                    For example
+                    I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
+                  </p>
+                  <p>
+                    I have successfully solved this problem with javascript files only.
+                    Try adding the async attribute in script tag or defer attribute
+                    For example
+                    I suggest you to use async, it loads the file only when needed. Defer attribute load the file at the end of the page, some time it will not perform its functionality required.
+                  </p>
+                </div>
+              ) : null
+            }
             <div className="col-md-4"></div>
           </div>
         </div>
